447c0dd08199796f96b848a6a4a76e8115f08571
De4oult <timofej.dimitrov@yandex.ru>
2023-06-27 22:01:09 +0300

    Hallo, welt

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,32 @@
 org 0x7C00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
+start:
+    jmp main
+
+puts:
+    ; save registers
+    push si
+    push ax
+
+.loop:
+    lodsb
+    or al, al
+    jz .done
+
+    mov ah, 0x0e
+    mov bh, 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
 main:
     ; data segments setup
     mov ax, 0
@@ -11,11 +37,16 @@ main:
     mov ss, ax
     mov sp, 0x7C00
 
+    mov si, msg_hello
+    call puts
+
     hlt
 
 .halt:
     jmp .halt
 
+
+msg_hello: db 'Hallo, Welt', ENDL, 0
 
 times 510-($-$$) db 0
 
